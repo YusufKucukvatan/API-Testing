ec49772f7d4e61c3a5036295bcb3faeee766e7a2
yusuf <yusuf@kucukvatan.com>
2020-01-23 11:41:59 -0500

    Second

--- src/test/java/com/automation/tests/day2/ORDSTest.java
+++ src/test/java/com/automation/tests/day2/ORDSTest.java
@@ -1,7 +1,13 @@
 package com.automation.tests.day2;
 
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*; //Those two static import helps us not to use class name to invoke that static methods...
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class ORDSTest {
-    public static void main(String[] args) {
+    @Test
+    public  void test1() {
 
     }
 }
